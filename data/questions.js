334f23ad35689ec2e13c64242a98cab7cc04c690
Steve <octopus@localhost.localdomain>
2018-02-01 18:57:29 +1300

    added data js file

---
+++ data/questions.js
@@ -0,0 +1,34 @@
+const questions = [
+  {
+    id:1,
+    question: 'Who pached your lunch today?',
+    options: ['Mum', 'Dad'],
+    answer: 'Dad'
+  },
+  {
+    id:2,
+    question: 'Who did Xmas better?',
+    options: ['Santa', 'Gunch'],
+    answer: 'Gunch'
+  },
+  {
+    id:3,
+    question: 'Do you think you are brave?',
+    options: ['Yes', 'No'],
+    answer: 'No'
+  },
+  {
+    id:4,
+    question: 'What do you think of voldemort?',
+    options: ['He is cool', 'He is evil'],
+    answer: 'He is cool'
+  },
+  {
+    id:5,
+    question: 'Hogwarts is under attact world you:',
+    options: ['Leave', 'Stay'],
+    answer: 'Stay'
+  }
+]
+
+export default questions
