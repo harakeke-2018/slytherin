1c24811e2ec9b6fc0e76a20ec5299d6a91b44fdf
EDA <kimmirindel97@gmail.com>
2018-02-02 08:48:59 +1300

    added pictures

--- data/questions.js
+++ data/questions.js
@@ -1,33 +1,33 @@
 const questions = [
   {
-    id:1,
-    question: 'Who pached your lunch today?',
+    id: 1,
+    question: 'Whose child are you more proud to be?',
     options: ['Mum', 'Dad'],
     answer: 'Dad'
   },
   {
-    id:2,
-    question: 'Who did Xmas better?',
-    options: ['Santa', 'Gunch'],
-    answer: 'Gunch'
+    id: 2,
+    question: 'Who did Christmas better?',
+    options: ['Santa', 'Grinch'],
+    answer: 'Grinch'
   },
   {
-    id:3,
+    id: 3,
     question: 'Do you think you are brave?',
     options: ['Yes', 'No'],
     answer: 'No'
   },
   {
-    id:4,
-    question: 'What do you think of voldemort?',
+    id: 4,
+    question: 'What do you think of Voldemort?',
     options: ['He is cool', 'He is evil'],
     answer: 'He is cool'
   },
   {
-    id:5,
-    question: 'Hogwarts is under attact world you:',
+    id: 5,
+    question: 'Hogwarts is under attack, would you:',
     options: ['Leave', 'Stay'],
-    answer: 'Stay'
+    answer: 'Leave and Stay'
   }
 ]
 
