873de99e251f8ec61f25dda45bb21e6c7a71ea00
catherinefromont <catherinegfromont@gmail.com>
2018-02-02 14:23:26 +1300

    Added score grab functionality for question, and ensured app scoreGrab is currently somewhat working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Home from './Home'
 import Questions from './Questions'
+import Result from './Result'
 import { HashRouter as Router, Route } from 'react-router-dom'
 
 class App extends React.Component {
   constructor (props) {
     super(props)
+    this.state = {
+      score: 0
+    }
+    this.scoreGrab = this.scoreGrab.bind(this)
+  }
+
+  scoreGrab (num) {
+    this.setState({
+      score: num
+    })
   }
 
   render () {
@@ -14,15 +25,20 @@ class App extends React.Component {
         <div className='container'>
           <div className='titleContainer'>
             <h1>Are you a true Slytherin?</h1>
+            {/* <h1>{this.state.score}</h1> */}
           </div>
           <main>
             <Route exact path='/' component={Home} />
-            <Route path='/questions' component={Questions} />
+            <Route path='/questions' render={() => {
+              return <Questions scoreGrab={this.scoreGrab} finalScore={this.state.score} />
+            }} />
+            <Route path='/result' component={Result} />
           </main>
         </div>
       </Router>
     )
   }
+
 }
 
 export default App
--- client/components/Questions.jsx
+++ client/components/Questions.jsx
@@ -1,40 +1,42 @@
 import React from 'react'
-import { Route, Link } from 'react-router-dom'
+import { Route, Link, Redirect } from 'react-router-dom'
 
 class Questions extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
+      finished: false,
       score: 0,
       testQs: [
         {
           question: 'Who did Christmas better?',
-          btn1: {isTrue: false, text: 'Santa'},
-          btn2: {isTrue: true, text: 'Grinch'}
+          btn1: { isTrue: false, text: 'Santa' },
+          btn2: { isTrue: true, text: 'Grinch' }
         },
         {
           question: 'Do you think you are brave?',
-          btn1: {isTrue: false, text: 'Yes'},
-          btn2: {isTrue: true, text: 'No'}
+          btn1: { isTrue: false, text: 'Yes' },
+          btn2: { isTrue: true, text: 'No' }
         },
         {
           question: 'What do you think of Voldemort?',
-          btn1: {isTrue: true, text: 'He is cool'},
-          btn2: {isTrue: false, text: 'He is evil'}
+          btn1: { isTrue: true, text: 'He is cool' },
+          btn2: { isTrue: false, text: 'He is evil' }
         },
         {
           question: 'Hogwarts is under attack, would you:',
-          btn1: {isTrue: true, text: 'Leave'},
-          btn2: {isTrue: true, text: 'Stay'}
+          btn1: { isTrue: true, text: 'Leave' },
+          btn2: { isTrue: true, text: 'Stay' }
         }
       ],
       currentQ: {
         question: 'Whose child are you more proud to be?',
-        btn1: {isTrue: false, text: 'Mum'},
-        btn2: {isTrue: true, text: 'Dad'}
+        btn1: { isTrue: false, text: 'Mum' },
+        btn2: { isTrue: true, text: 'Dad' }
       }
     }
     this.updatePageandTotal = this.updatePageandTotal.bind(this)
+    this.scoreGrab = this.props.scoreGrab.bind(this)
   }
 
   updatePageandTotal (num) {
@@ -44,11 +46,17 @@ class Questions extends React.Component {
         currentQ: this.state.testQs.shift()
       })
     } else {
-      // pass score + num to next function
+      this.props.scoreGrab(this.state.score + num)
+      this.setState({
+        finished: true
+      })
     }
   }
 
   render () {
+    if (this.state.finished) {
+      return <Redirect to='/result' />
+    }
     return (
       <div className='middleContainer'>
         <div className='content'>
